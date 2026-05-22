416eb123b9e2271a50db8b980951508590399da3
IvanGrabovsky <ivan.grabovsky.ua@gmail.com>
2026-05-22 16:07:21 +0300

    add interaction with the camera

--- App.js
+++ App.js
@@ -1,20 +1,46 @@
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet } from 'react-native';
-import AllPlaces from './screens/AllPlaces';
-import AddPlace from './screens/AddPlace';
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet } from "react-native";
+import AllPlaces from "./screens/AllPlaces";
+import AddPlace from "./screens/AddPlace";
+import IconButton from "./components/UI/IconButton";
+import { Colors } from "./constants/colors";
 
-const Stack = createNativeStackNavigator()
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <>
       <StatusBar style="dark" />
       <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen name='AllPlaces' component={AllPlaces} />
-          <Stack.Screen name='AddPlace' component={AddPlace} />
+        <Stack.Navigator
+          screenOptions={{
+            headerStyle: { backgroundColor: Colors.primary500 },
+            headerTintColor: Colors.gray700,
+            contentStyle: { backgroundColor: Colors.gray700 },
+          }}
+        >
+          <Stack.Screen
+            name="AllPlaces"
+            component={AllPlaces}
+            options={({ navigation }) => ({
+              title: "Your Favorite Places",
+              headerRight: (tintColor) => (
+                <IconButton
+                  icon="add"
+                  color={tintColor}
+                  size={24}
+                  onPress={() => navigation.navigate("AddPlace")}
+                />
+              ),
+            })}
+          />
+          <Stack.Screen
+            name="AddPlace"
+            component={AddPlace}
+            options={{ title: "Add a New Place" }}
+          />
         </Stack.Navigator>
       </NavigationContainer>
     </>
---
+++ components/Places/ImagePicker.js
@@ -0,0 +1,24 @@
+import { launchCameraAsync } from "expo-image-picker";
+import { Button, StyleSheet, View } from "react-native";
+
+function ImagePicker() {
+  async function takeImageHandler() {
+    const image = await launchCameraAsync({
+      allowsEditing: true,
+      aspect: [16, 9],
+      quality: 0.5,
+    });
+    console.log(image);
+  }
+
+  return (
+    <View>
+      <View></View>
+      <Button title="Take Image" onPress={takeImageHandler} />
+    </View>
+  );
+}
+
+export default ImagePicker;
+
+const styles = StyleSheet.create({});
